07d06a5360b097627d348f628bb2d2d2b2562c6c
abhigulve <abhigulve06@gmail.com>
2018-05-11 20:48:23 +0530

    dell hiring challenge questions

--- src/main/java/challenges/dell/EqualString.java
+++ src/main/java/challenges/dell/EqualString.java
@@ -1,4 +1,4 @@
-package challenges.thoughtworks.dell;
+package challenges.dell;
 
 import java.util.Scanner;
 
--- src/main/java/challenges/dell/OneSwapPalindrome.java
+++ src/main/java/challenges/dell/OneSwapPalindrome.java
@@ -1,4 +1,4 @@
-package challenges.thoughtworks.dell;
+package challenges.dell;
 
 import java.util.Arrays;
 import java.util.Scanner;
