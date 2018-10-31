6883b2953d3b05accada8b346d559b518ba8aa30
gulve <abhijeet.gulve@tomtom.com>
2018-10-31 20:33:55 +0530

    added node class in AddTwoLinkedList class

--- src/main/java/AddTwoLinkedList.java
+++ src/main/java/AddTwoLinkedList.java
@@ -3,5 +3,30 @@
  */
 public class AddTwoLinkedList {
 
+class Node<T> {
+    Node next;
+    T data;
 
+    public Node(Node next, T data) {
+        this.next = next;
+        this.data = data;
+    }
+
+    public Node getNext() {
+
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
 }
