0c24c82408ab4561bbb849a0b063d77f7ffd0ee3
gulve <abhijeet.gulve@tomtom.com>
2018-11-30 17:34:45 +0530

    added code for SubSetWithGivenSum

---
+++ src/main/java/dp/SubSetWithGivenSum.java
@@ -0,0 +1,26 @@
+package dp;
+
+/**
+ * @author Abhijeet Gulve
+ */
+public class SubSetWithGivenSum {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 4, 6};
+        System.out.println(getGivenSumSubSet(array, 6, 3));
+    }
+
+    private static int getGivenSumSubSet(int[] array, int sum, int i) {
+        if (i < 0) {
+            return 0;
+        }
+        if (sum == 0) {
+//            System.out.println(1);
+            return 1;
+        } else if (sum < array[i]) {
+            return getGivenSumSubSet(array, sum, i - 1);
+        } else {
+            return getGivenSumSubSet(array, sum - array[i], i - 1) + getGivenSumSubSet(array, sum, i - 1);
+        }
+    }
+
+}
